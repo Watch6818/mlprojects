c1104f192ce80a6f77a8935ea6c9c4a149d8fd9b
Watch6818 <garrettdavis1536@gmail.com>
2024-10-04 13:40:52 -0600

    1) rewrote to match textbook code exactly 2) changed "lr" to "learning_rate" 3) added "x_scalar" and "predicted_y_scalar" to extract values from np array

--- exam_scores.py
+++ exam_scores.py
@@ -1,29 +1,33 @@
 import numpy as np
-from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense
 from keras import optimizers
 
 data = np.array([[1.4, 20.], [1.8, 30.], [2.1, 37.], [2.2, 45.],
 [2.3,26.], [2.9, 86.], [3.5, 67.], [3.6, 100.],
 [4.2, 72.], [4.6, 82.], [4.7, 99.]])
 
-[X_train,Y_train] = data.transpose()
+[X_train, Y_train] = data.transpose()
+model = Sequential()
+model.add(Dense(1, activation='linear', input_dim=1))
+SGD = optimizers.SGD(learning_rate=0.03)
+model.compile(loss='mean_squared_error', optimizer=SGD)
 
-model = keras.Sequential()
-model.add(keras.Dense(1, activation='linear', input_dim=1))
-SGD = optimizers.SGD(lr=0.03) #lr is the learning rate
-model.compile(loss='mean_squared_error',optimizer=SGD)
-
-#this command does the minimization
+# this command does the minimization
 model.fit(X_train, Y_train, epochs=100, verbose=1)
 
-#displays some info, note there are only 2 fitting parameters
+# displays some info, note there are only 2 fitting parameters
 model.summary()
 
 # the predict function on the next line assumes an
 # array of inputs so we need to put 2.7 inside an array
 x = np.array([2.7])
 
-#... and predicted_y is likewise an array of predicted outputs
+# ... and predicted_y is likewise an array of predicted outputs
 predicted_y = model.predict(x)
 
-print(f'For x={x:f} the predicted y is: {predicted_y:f}')
+# Extract scalar values from the arrays
+x_scalar = x.item()
+predicted_y_scalar = predicted_y.item()
+
+print(f'For x={x_scalar:f} the predicted y is: {predicted_y_scalar:f}')
